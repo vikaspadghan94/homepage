d6012207d6ae6695856e75cb75243a1c98e6c422
vikaspadghan94 <vikaspadghan94@gmail.com>
2024-06-17 14:28:41 +0530

    Describe your changes here

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,3 @@
-import basket_icon from './basket_icon.png'
-import logo from './logo.png'
-import header_img from './header_img.png'
-import search_icon from './search_icon.png'
 import menu_1 from './menu_1.jpg'
 import menu_2 from './menu_1.jpg'
 import menu_3 from './menu_1.jpg'
@@ -18,21 +14,13 @@ import play_store from './play_store.png'
 import linkedin_icon from './linkedin_icon.png'
 import facebook_icon from './facebook_icon.png'
 import twitter_icon from './twitter_icon.png'
-import selector_icon from './selector_icon.png'
-import rating_starts from './rating_starts.png'
 
 export const assets = {
-    logo,
-    basket_icon,
-    header_img,
-    search_icon,
-    rating_starts,
     app_store,
     play_store,
     linkedin_icon,
     facebook_icon,
     twitter_icon,
-    selector_icon,
     tiffin,
     plate
 }
